2cadaf57e133e0ceb86a7478a9e9bbc7b7dec555
Krittamate <krittamateza@gmail.com>
2022-06-25 21:38:27 +0700

    WIP Upload Worksheet

--- app/JsonConverter/ExceltoJson.js
+++ app/JsonConverter/ExceltoJson.js
@@ -6,8 +6,18 @@ const fs = require('fs');
 const Table = db.Project_Table;
 
 exports.excelJson = (req, res) => {
-    const cell = new Table({
+    const cell = new Table
+    ({
         project_name:req.body.project_name,
 
+    })
+    cell.save((err, cell) =>{
+        if(err){
+            res.status(500).send({ message: err });
+            return;
+        }
+        if(req.body.roles){
+            
+        }
     })
 }
--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -10,7 +10,6 @@ module.exports = function (app) {
     next();
   });
   app.get("/api/test/all", controller.allAccess);
-  app.get("/api/test/a", controller.allAccess);
   app.get("/api/test/user", [authJwt.verifyToken], controller.userBoard);
   app.get(
     "/api/test/mod",
@@ -22,4 +21,8 @@ module.exports = function (app) {
     [authJwt.verifyToken, authJwt.isAdmin],
     controller.adminBoard
   );
+  app.post("/api/test/upload_sheet",
+  [authJwt.verifyToken, authJwt.isAdmin], 
+  controller.allAccess
+);
 };
